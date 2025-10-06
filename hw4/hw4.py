ae1fc504138eae32fddb118b9ec328fdc3e8c348
Oscar <orss4123@gmail.com>
2025-10-06 00:38:17 -0400

    absurd-trolley-problems

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -60,27 +60,57 @@ def neal_fun_paper(driver):
         time.sleep(1)
         neal_fun_paper_items(driver)
 
+def neal_fun_trolly(driver):
+    driver.get('https://neal.fun/absurd-trolley-problems/')
+    for _ in range(28):
+        time.sleep(5)
+        level_info = driver.find_element(By.CSS_SELECTOR, 'div.level').text.split()
+        level = int(level_info[1][:-1])
+        level_name = ' '.join(level_info[2:])
+        wavy_text_words = driver.find_elements(By.CSS_SELECTOR, 'div.wavy-text span.wavy-text-word')
+        words = []
+        for wavy_word in wavy_text_words[3:]:
+            words.append(''.join([x.text for x in wavy_word.find_elements(By.CSS_SELECTOR, 'span.letter')]))
+        dilemma = ' '.join(words).strip()
+        button = driver.find_elements(By.CSS_SELECTOR, 'button.action')[0]
+        button.click()
+        time.sleep(5)
+        votes_info_spans = driver.find_elements(By.CSS_SELECTOR, 'div.next-wrapper div.stat span.wavy-text-word')
+        pulled_level = ''.join([x.text for x in votes_info_spans[0].find_elements(By.CSS_SELECTOR, 'span.letter')])
+        not_pulled_level = ''.join([x.text for x in votes_info_spans[6].find_elements(By.CSS_SELECTOR, 'span.letter')])
+        votes = int(''.join([x.text for x in votes_info_spans[8].find_elements(By.CSS_SELECTOR, 'span.letter') if x.text != '(' and x.text != ',']))
+        data = {
+            'url': 'https://neal.fun/absurd-trolley-problems/',
+            'title': 'Absurd Trolley Problems',
+            'level': level,
+            'level name': level_name,
+            'dilemma': dilemma,
+            'pulled lever': pulled_level,
+            'not pulled lever': not_pulled_level,
+            'votes': votes
+        }
+        json_line = json.dumps(data)
+        with open(json_lines_file, 'a') as fp:
+            fp.write(json_line + '\n')
+        next_button = driver.find_element(By.CSS_SELECTOR, 'button.action.action-next')
+        next_button.click()
 
 user_agent = os.getenv('USER_AGENT')
 HEADERS = {'user-agent': user_agent}
+driver = webdriver.Chrome()
 
 urls = [
-    ('https://neal.fun/', neal_fun_paper),
+    ('https://neal.fun/', 'https://neal.fun/paper/', neal_fun_paper),
+    ('https://neal.fun/', 'https://neal.fun/absurd-trolley-problems/', neal_fun_trolly)
 ]
 
-for url, fun in urls:
+for url, index, fun in urls:
     responce = requests.get(url + '/robots.txt', headers=HEADERS)
     rp = Protego.parse(responce.text)
     crawl_delay = rp.crawl_delay(user_agent) or 0
-    fetch_status = rp.can_fetch(url, user_agent=user_agent)
+    fetch_status = rp.can_fetch(index, user_agent=user_agent)
 
     if fetch_status:
+        fun(driver)
 
-        driver = webdriver.Chrome()
-        try:
-            fun(driver)
-        except:
-            print('Error happened')
-        
-
-    driver.quit()
+driver.quit()
