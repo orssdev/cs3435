b3b364f2237c62a666eef9b99c88c12ee2a74fb1
Oscar <orss.dev@gmail.com>
2025-10-20 11:49:38 -0400

    Starting get_row

--- hw5/p06.py
+++ hw5/p06.py
@@ -8,13 +8,29 @@ def construct_file_name(lat, lon):
     file_name = f'USGS_NED_1_{'n' if lat > 0 else 's'}{f'{abs(lat):02}'}{'e' if lon > 0 else 'w'}{f'{abs(lon):03}'}_IMG.tif'
     return file_name
 
-print(construct_file_name(lat=36, lon=-82))
-print(construct_file_name(lat=-36, lon=82))
-print(construct_file_name(lat=-0, lon=0))
 
-# im = plt.imread('USGS_NED_1_n36w082_IMG.tif')
-# print(type(im))
-# print(im.shape)
+def load_trim_image(lat, lon):
+    """ Takes the latitude and longitude as signed integers and
+    loads the appropriate file. It then trims off the boundary
+    of six pixels on all four sides. """
+    image = plt.imread(construct_file_name(lat, lon))
+    return image[6:-6, 6:-6]
 
-# plt.imshow(im)
-# plt.show()
+def stitch_four(lat, lon):
+    # load the four images and construct the resulting image:
+    # (nw_lat, nw_lon), (nw_lat, nw_lon+1)
+    # (nw_lat-1, nw_lon), (nw_lat-1, nw_lon+1)
+    image1 = load_trim_image(lat, lon)
+    image2 = load_trim_image(lat, lon + 1)
+    image3 = load_trim_image(lat - 1, lon)
+    image4 = load_trim_image(lat - 1, lon + 1)
+    top = np.concatenate([image1, image2], axis=1)
+    bottom = np.concatenate([image3, image4], axis=1)
+    image = np.concatenate([top, bottom])
+    return image
+
+def get_row(lat, lon_min, num_tiles):
+    """ Takes the latitude, minimum longitude, and number of tiles and
+    returns an image that combines tiles along a row of different
+    longitudes. """
+    return image
